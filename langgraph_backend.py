885be563667dd64693062e459f1514d81f6c4abd
Yunish <111888546+Itsyunish@users.noreply.github.com>
2025-09-01 10:56:53 +0545

    Update langgraph_backend.py

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -6,6 +6,7 @@ from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 
+# reading .env file
 load_dotenv()
 
 llm = ChatOpenAI()
@@ -27,3 +28,4 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
